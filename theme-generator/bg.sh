9f14745db37e2acd7fb833db60ac25d18fff41aa
Alexis Besson <alexis.besson@carrier.com>
2021-09-06 22:42:18 +0200

    theme generator

--- theme-generator/bg.sh
+++ theme-generator/bg.sh
@@ -1,167 +1,44 @@
 #!/bin/bash
 
-colorAvg=''
+~/.nvm/versions/node/v14.8.0/bin/node ~/Development/dotfiles/theme-generator/index.js > ~/Development/dotfiles/theme-generator/output.txt
+feh --bg-fill ~/Development/dotfiles/theme-generator/trianglify.png
 
-darker=''
-lighter=''
-primary=''
-secondary=''
+primary=$(head -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 1)
+secondary=$(head -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 2)
+lighter=$(head -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 3)
+darker=$(head -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 4)
+term=$(head -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 5)
 
-get_color_hunt_palettes () {
-    #res=`wget -qO- --no-check-certificate --method POST --body-data 'sort=popular&step=0&timeframe=4000' 'https://colorhunt.co/php/feed.php'`
-    #echo $res > res.txt
-    res=`cat res.txt`
-}
+color0=$(tail -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 1)
+color1=$(tail -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 2)
+color2=$(tail -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 3)
+color3=$(tail -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 4)
+color4=$(tail -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 5)
+color5=$(tail -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 6)
+color6=$(tail -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 7)
+color7=$(tail -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 8)
+color8=$(tail -n 1 ~/Development/dotfiles/theme-generator/scheme | cut -d ' ' -f 9)
 
-get_color_average () {
-    local colorRHex="`echo $1 | cut -c2-3`"
-    local colorGHex="`echo $1 | cut -c4-5`"
-    local colorBHex="`echo $1 | cut -c6-7`"
-    local colorRDec="`echo $((16#$colorRHex))`"
-    local colorGDec="`echo $((16#$colorGHex))`"
-    local colorBDec="`echo $((16#$colorBHex))`"
+polybar_config_template=`cat ~/Development/dotfiles/polybar/config`
+echo "$polybar_config_template" | sed -e "s/secondary = #[a-zA-Z0-9]*$/secondary = $primary/g" -e "s/primary = #[a-zA-Z0-9]*$/primary = $lighter/g" > ~/Development/dotfiles/polybar/config
 
-    colorAvr=$((($colorRDec + $colorGDec + $colorBDec) / 3 ))
-}
+rofi_config_template=`cat ~/Development/dotfiles/rofi/dark.theme.template`
+echo "$rofi_config_template" | sed -e "s/@PRIMARY/$primary/g" -e "s/@DARKER/$darker/g" > ~/Development/dotfiles/rofi/dark.theme
 
-assign_colors () {
-    local averages=($color1Avr $color2Avr $color3Avr $color4Avr)
-    local sorted=($(echo ${averages[*]}| tr " " "\n" | sort -n))
+alacritty_config_template=`cat ~/Development/dotfiles/alacritty/alacritty.template.yml`
+echo "$alacritty_config_template" | sed -e 's/^    background: "#[a-zA-Z0-9]\{6\}"$/    background: "'"$term"'"/g' -e 's/^    black:   "#[a-zA-Z0-9]\{6\}"$/    black:   "'"$color0"'"/g' -e 's/^    red:     "#[a-zA-Z0-9]\{6\}"$/    red:     "'"$color1"'"/g' -e 's/^    green:   "#[a-zA-Z0-9]\{6\}"$/    green:   "'"$color2"'"/g' -e 's/^    yellow:  "#[a-zA-Z0-9]\{6\}"$/    yellow:  "'"$color3"'"/g' -e 's/^    blue:    "#[a-zA-Z0-9]\{6\}"$/    blue:    "'"$color4"'"/g' -e 's/^    magenta: "#[a-zA-Z0-9]\{6\}"$/    magenta: "'"$color5"'"/g' -e 's/^    cyan:    "#[a-zA-Z0-9]\{6\}"$/    cyan:    "'"$color6"'"/g' -e 's/^    white:   "#[a-zA-Z0-9]\{6\}"$/    white:   "'"$color7"'"/g' > ~/Development/dotfiles/alacritty/alacritty.yml
 
-    if [[ "${sorted[0]}" == "${averages[0]}" ]]; then
-        darker=$color1
-    fi
-    if [[ "${sorted[0]}" == "${averages[1]}" ]]; then
-        darker=$color2
-    fi
-    if [[ "${sorted[0]}" == "${averages[2]}" ]]; then
-        darker=$color3
-    fi
-    if [[ "${sorted[0]}" == "${averages[3]}" ]]; then
-        darker=$color4
-    fi
-
-    if [[ "${sorted[3]}" == "${averages[0]}" ]]; then
-        lighter=$color1
-    fi
-    if [[ "${sorted[3]}" == "${averages[1]}" ]]; then
-        lighter=$color2
-    fi
-    if [[ "${sorted[3]}" == "${averages[2]}" ]]; then
-        lighter=$color3
-    fi
-    if [[ "${sorted[3]}" == "${averages[3]}" ]]; then
-        lighter=$color4
-    fi
-        diff1=$((${sorted[1]} - 128))
-        diff2=$((${sorted[2]} - 128))
-        abs1=${diff1#-}
-        abs2=${diff2#-}
-        if ((abs1 <= abs2)); then
-            if [[ "${sorted[1]}" == "${averages[0]}" ]]; then
-                primary=$color1
-            fi
-            if [[ "${sorted[1]}" == "${averages[1]}" ]]; then
-                primary=$color2
-            fi
-            if [[ "${sorted[1]}" == "${averages[2]}" ]]; then
-                primary=$color3
-            fi
-            if [[ "${sorted[1]}" == "${averages[3]}" ]]; then
-                primary=$color4
-            fi
-
-            if [[ "${sorted[2]}" == "${averages[0]}" ]]; then
-                secondary=$color1
-            fi
-            if [[ "${sorted[2]}" == "${averages[1]}" ]]; then
-                secondary=$color2
-            fi
-            if [[ "${sorted[2]}" == "${averages[2]}" ]]; then
-                secondary=$color3
-            fi
-            if [[ "${sorted[2]}" == "${averages[3]}" ]]; then
-                secondary=$color4
-            fi
-        else
-            if [[ "${sorted[2]}" == "${averages[0]}" ]]; then
-                primary=$color1
-            fi
-            if [[ "${sorted[2]}" == "${averages[1]}" ]]; then
-                primary=$color2
-            fi
-            if [[ "${sorted[2]}" == "${averages[2]}" ]]; then
-                primary=$color3
-            fi
-            if [[ "${sorted[2]}" == "${averages[3]}" ]]; then
-                primary=$color4
-            fi
-
-            if [[ "${sorted[1]}" == "${averages[0]}" ]]; then
-                secondary=$color1
-            fi
-            if [[ "${sorted[1]}" == "${averages[1]}" ]]; then
-                secondary=$color2
-            fi
-            if [[ "${sorted[1]}" == "${averages[2]}" ]]; then
-                secondary=$color3
-            fi
-            if [[ "${sorted[1]}" == "${averages[3]}" ]]; then
-                secondary=$color4
-            fi
-        fi
-}
-
-
-res=''
-get_color_hunt_palettes
-
-palettes="`echo $res | sed 's/\"/\n/g' | grep '[a-z0-9]\{24\}'`"
-qty="`echo $palettes | wc -w`"
-random=$((1 + $RANDOM % $qty))
-
-palette="`echo $palettes | cut -d " " -f $random`"
-color1="#`echo $palette | cut -c1-6`"
-color2="#`echo $palette | cut -c7-12`"
-color3="#`echo $palette | cut -c13-18`"
-color4="#`echo $palette | cut -c19-24`"
-
-echo '--- BASE ---' 
-echo $color1
-echo $color2
-echo $color3
-echo $color4
-
-get_color_average $color1
-color1Avr=$colorAvr
-
-get_color_average $color2
-color2Avr=$colorAvr
-
-get_color_average $color3
-color3Avr=$colorAvr
-
-get_color_average $color4
-color4Avr=$colorAvr
-
-echo '--- Averages ---' 
-echo $color1Avr
-echo $color2Avr
-echo $color3Avr
-echo $color4Avr
-
-assign_colors
-
-
-echo "Darker : $darker"
-echo "Lighter : $lighter"
 echo "Primary : $primary"
 echo "Secondary : $secondary"
+echo "Lighter : $lighter"
+echo "Darker : $darker"
+echo "Term : $term"
 
-~/.nvm/versions/node/v14.8.0/bin/node ~/Development/dotfiles/theme-generator/index.js $darker $primary $secondary $lighter
-feh --bg-fill /home/lab3ss/.backgrounds/trianglify.png
-
-#i3_rofi_template=`cat ~/Development/dotfiles/polybar/config`
-
-#echo "$i3_rofi_template" |
-#	sed -e "s/primary = #[a-zA-Z0-9]*$/primary = $color1/g" > ~/Development/dotfiles/polybar/config
+echo "Color0: $color0"
+echo "Color1: $color1"
+echo "Color2: $color2"
+echo "Color3: $color3"
+echo "Color4: $color4"
+echo "Color5: $color5"
+echo "Color6: $color6"
+echo "Color7: $color7"
--- theme-generator/index.js
+++ theme-generator/index.js
@@ -1,20 +1,259 @@
+const Color = require('color')
+const axios = require('axios')
+const formData = require('form-data');
 const trianglify = require('trianglify')
 const fs = require('fs')
+const path = require('path')
 
-const colors = process.argv.slice(2);
+const mock = [
+  '222831393e4600adb5eeeeee',
+  'f9ed69f08a5db83b5e6a2c70',
+  'f38181fce38aeaffd095e1d3',
+  '08d9d6252a34ff2e63eaeaea',
+  'e3fdfdcbf1f5a6e3e971c9ce',
+  'a8d8eaaa96dafcbad3ffffd2',
+  'ffc7c7ffe2e2f6f6f68785a2',
+  '364f6b3fc1c9f5f5f5fc5185',
+  'f9f7f7dbe2ef3f72af112d4e',
+  'ffb6b9fae3d9bbded661c0bf',
+  'e4f9f530e3ca11999e40514e',
+  'defcf9cadefcc3bef0cca8e9',
+  '2b2e4ae8454590374953354a',
+  'f67280c06c846c5b7b355c7d',
+  '00b8a9f8f3d4f6416cffde7d',
+  'e23e5788304e522546311d3f',
+  'ffcfdffefdcae0f9b5a5dee5',
+  'a8e6cfdcedc1ffd3b6ffaaa5',
+  '2121213232320d737714ffec',
+  'bad7dfffe2e2f6f6f699ddcc',
+  '48466d3d84a846cdcfabedd8',
+  'ffc8c8ff9999444f5a3e4149',
+  '2d4059ea5455f07b3fffd460',
+  '1b262c0f4c753282b8bbe1fa',
+  '3ec1d3f6f7d7ff9a00ff165d',
+  'f4eeffdcd6f7a6b1e1424874',
+  'a1eafbfdfdfdffcef3cabbe9',
+  'f0f5f9c9d6df52616b1e2022',
+  '6fe7dd3490de6639a6521262',
+  'f7fbfcd6e6f2b9d7ea769fcd',
+  '8ef6e49896f1d59bf6edb1f1',
+  'd4a5a5ffecdaf9ffeaa6d0e4',
+  '30384100adb5eeeeeeff5722',
+  '1fab8962d2a29df3c4d7fbe8',
+  'a8e6cffdffabffd3b6ffaaa5',
+  'f5eee6f3d7cae6a4b4c86b85',
+  '07689fa2d5f2fafafaff7e67',
+  'dcedc2ffd3b5ffaaa6ff8c94',
+  'f85f73fbe8d3928a97283c63',
+  '3038413a4750d72323eeeeee'
+]
+
+const getColorAverage = (color) => {
+        const c = color.rgb().array()
+        return (c.reduce((a, b) => a + b, 0) / c.length)
+}
+
+const extractColors = (string) => {
+    let colors = [
+        Color(`#${string.substring(0, 6)}`),
+        Color(`#${string.substring(6, 12)}`),
+        Color(`#${string.substring(12, 18)}`),
+        Color(`#${string.substring(18, 24)}`)
+    ]
+
+    // Extract primary color
+
+    let primaryIndex = 0
+    let biggerGap = 0
+
+    for (let i = 0; i < colors.length; i++) {
+        const c = colors[i].rgb().array().sort()
+        const gap = c[2] - c[0]
+        if (gap > biggerGap) {
+            biggerGap = gap
+            primaryIndex = i
+        }
+    }
+
+    const primary = colors[primaryIndex]
+    colors = colors.filter((c, i) => i !== primaryIndex)
+
+    // Extract lighter color
+
+    let lighterIndex = 0
+    let lighterAvg = 0
+
+    for (let i = 0; i < colors.length; i++) {
+        const avg = getColorAverage(colors[i])
+        if (avg > lighterAvg) {
+            lighterAvg = avg
+            lighterIndex = i
+        }
+    }
+
+    const lighter = colors[lighterIndex]
+    colors = colors.filter((c, i) => i !== lighterIndex)
+
+    // Extract darker color
+
+    let darkerIndex = 0
+    let darkerAvg = 255
+
+    for (let i = 0; i < colors.length; i++) {
+        const avg = getColorAverage(colors[i])
+        if (avg < darkerAvg) {
+            darkerAvg = avg
+            darkerIndex = i
+        }
+    }
+
+    const darker = colors[darkerIndex]
+    colors = colors.filter((c, i) => i !== darkerIndex)
+
+    // Extract secondary color
+
+    const secondary = colors[0]
+    
+    return { primary, secondary, lighter, darker }
+}
+
+const getColors = new Promise((res, rej) => {
+
+    res(mock)
+    /*
+    // Fetch top colors schemes from colorhunt.co
+    const url = 'https://colorhunt.co/php/feed.php'
+    const data = new formData();
+
+    data.append('sort', 'popular');
+    data.append('step', '0');
+    data.append('timeframe', '4000');
+
+    const config = {
+      method: 'post',
+      url,
+      headers: { 
+        ...data.getHeaders()
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+        if (response && response.data) {
+            const sorted = response.data
+                .sort((a, b) => parseInt(a.likes, 10) > parseInt(b.likes, 10) ? -1 : 1)
+                // .map(e => e.code)
+            console.log(sorted)
+        }
+    })
+    .catch(function (error) {
+      console.log(`Error fetching colors from colorhunt.co : ${error}`);
+        rej(error)
+    });
+    */
+    
+})
+
+const schemeFilter = (scheme) => {
+    return true
+
+    const { primary, secondary, lighter, darker } = extractColors(scheme)
+    
+    const primaryAvg = getColorAverage(primary)
+    if (primaryAvg < 40 || primaryAvg > 210) {
+        console.info('eliminated because of primary', primaryAvg)
+        return false
+    }
+
+    const lighterAvg = getColorAverage(lighter)
+    if (lighterAvg < 130 || lighterAvg < primaryAvg) {
+        console.info('eliminated because of lighter', lighterAvg)
+        return false
+    }
+
+    const darkerAvg = getColorAverage(darker)
+    if (darkerAvg > 120 || darkerAvg > primaryAvg) {
+        console.info('eliminated because of darker', darkerAvg)
+        return false
+    }
+    return true
+}
+
+const selectScheme = (schemes) => {
+    console.log('BEFORE FILTERS ', schemes.length)
+    const candidates = schemes.filter(schemeFilter)
+    console.log('AFTER FILTERS ', candidates.length)
+    const index = Math.floor(Math.random() * candidates.length)
+    return extractColors(candidates[index])
+}
+
+const scheme = selectScheme(mock)
+
+// 4 main colors from selected scheme
+const primary = scheme.primary.hex()
+const secondary = scheme.secondary.hex()
+const lighter = scheme.lighter.hex()
+const darker = scheme.darker.hex()
+
+const baseBlack = Color('#000000')
+const baseWhite = Color('#FFFFFF')
+
+// One terminal bg color
+let terminal = scheme.darker.mix(baseBlack, 0.2)
+while(getColorAverage(terminal) > 30) {
+    terminal = terminal.mix(baseBlack, 0.1)
+}
+
+// Extract 8 font colors from scheme
+
+let blackLight = terminal.mix(baseWhite, 0.1)
+
+let white = scheme.lighter.mix(baseWhite, 0.2)
+while(getColorAverage(white) < 225) {
+    white = white.mix(baseWhite, 0.2)
+}
+
+let primaryColor = Color(primary)
+while(getColorAverage(primaryColor) < 128) {
+    primaryColor = primaryColor.mix(baseWhite, 0.1)
+}
+
+let secondaryColor = Color(secondary)
+while(getColorAverage(secondaryColor) < 128) {
+    secondary = secondaryColor.mix(baseWhite, 0.1)
+}
+
+const color0 = blackLight.hex() // black light
+const color7 = white.hex() // white
+const color2 = primaryColor.hex()     // green
+const color1 = secondaryColor.hex()   // red
+const color5 = lighter   // magenta
+const color6 = scheme.darker.mix(baseWhite, 0.4).hex()  // cyan
+const color3 = primaryColor.mix(baseWhite, 0.2).hex()   // yellow
+const color4 = secondaryColor.mix(baseWhite, 0.2).hex()   // yellow
+
+/*
+getColors().then((schemes) => {
+    const scheme = selectScheme(schemes)
+
+})
+
+*/
 
 const canvas = trianglify({
   width: 2560,
   height: 1440,
   cellSize: 400,
   variance: 0.5,
-  xColors: colors,
+  xColors: [darker, primary, secondary, lighter],
   yColors: 'match',
 }).toCanvas()
 
-try {
-    const file = fs.createWriteStream('/home/lab3ss/.backgrounds/trianglify.png')
-    canvas.createPNGStream().pipe(file)
-} catch (e){
-    console.error(e)
-}
+const bgPath = path.join(__dirname, 'trianglify.png')
+const file = fs.createWriteStream(bgPath)
+canvas.createPNGStream().pipe(file)
+
+const schemePath = path.join(__dirname, 'scheme')
+const schemeFileContent = `${primary} ${secondary} ${lighter} ${darker} ${terminal.hex()}\n${color0} ${color1} ${color2} ${color3} ${color4} ${color5} ${color6} ${color7}`
+fs.writeFileSync(schemePath, schemeFileContent, 'UTF-8')
