0287d5062c8cb17746b8b37cb2d9751c6e948daf
Alexis Besson <alexis@besson.tech>
2022-03-30 21:07:05 +0200

    adapt dark theme on polybar and rofi

--- pscircle/makeBG.sh
+++ pscircle/makeBG.sh
@@ -2,21 +2,21 @@
 
 pscircle \
     --output=./pscircle.jpg \
-    --background-color=928A97 \
-    --link-color-min=283C6350 \
-    --link-color-max=283C6370 \
-    --dot-color-min=283C63 \
-    --dot-color-max=283C63 \
+		--background-color=191d26 \
+		--link-color-min=F97F8F \
+		--link-color-max=F97F8F \
+		--dot-color-min=F85F73 \
+		--dot-color-max=F85F73 \
     --tree-font-size=18 \
-    --tree-font-color=FBE8D3 \
+		--tree-font-color=c5bfc5 \
     --tree-font-face="DejaVu Sans Mono" \
     --toplists-row-height=35 \
     --toplists-font-size=24 \
-    --toplists-font-color=FBE8D3 \
-    --toplists-pid-font-color=283C63 \
+		--toplists-font-color=c5bfc5 \
+		--toplists-pid-font-color=F85F73 \
     --toplists-font-face="DejaVu Sans Mono" \
     --toplists-bar-height=7 \
-    --toplists-bar-background=F85F73 \
-    --toplists-bar-color=283C63;
+    --toplists-bar-background=c5bfc5 \
+    --toplists-bar-color=F85F73;
 
 feh --bg-fill ./pscircle.jpg;
--- pscircle/makeBG2.bk.sh
+++ pscircle/makeBG2.bk.sh
@@ -3,22 +3,22 @@
 while true; do
 	pscircle \
 		--output=./pscircle.jpg \
-		--background-color=1d1d1d \
-		--link-color-min=00AA8830 \
-		--link-color-max=00AA8870 \
-		--dot-color-min=00AA88 \
-		--dot-color-max=00AA88 \
+		--background-color=191d26 \
+		--link-color-min=00F85F73 \
+		--link-color-max=00F85F73 \
+		--dot-color-min=F85F73 \
+		--dot-color-max=F85F73 \
 		--tree-font-size=18 \
 		--tree-font-color=c5bfc5 \
 		--tree-font-face="DejaVu Sans Mono" \
 		--toplists-row-height=35 \
 		--toplists-font-size=24 \
 		--toplists-font-color=c5bfc5 \
-		--toplists-pid-font-color=00AA88 \
+		--toplists-pid-font-color=F85F73 \
 		--toplists-font-face="DejaVu Sans Mono" \
 		--toplists-bar-height=7 \
 		--toplists-bar-background=444444 \
-		--toplists-bar-color=00AA88;
+		--toplists-bar-color=F85F73;
 
 	# feh --bg-fill ~/Pictures/wallpapers/pscircle.jpg;
 	sleep 10;
