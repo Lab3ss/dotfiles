e7f4bb6dd6ac5e35be47c27a6a027b1c6fb9dfcf
Alexis Besson <alexis@besson.tech>
2023-05-12 10:26:53 +0200

    save it or lose it

--- background.sh
+++ background.sh
@@ -6,8 +6,8 @@ while true; do
   echo $TIME
   echo $DAY
   echo $DATE
-  convert -font "Chilanka-Regular" -gravity Center -fill "#ffffff30" -pointsize 260 -draw "text -940,-640 '$TIME'" /home/alexis/Pictures/background/main.png /home/alexis/Pictures/background/generated.png
-  convert -font "Chilanka-Regular" -gravity Center -fill "#ffffff30" -pointsize 60 -draw "text -940,-490 '$DATE'" /home/alexis/Pictures/background/generated.png /home/alexis/Pictures/background/generated.png
+  convert -font "Chilanka-Regular" -gravity Center -fill "#ffffff30" -pointsize 260 -draw "text -1480,-640 '$TIME'" /usr/share/backgrounds/warty-final-ubuntu.png /home/alexis/Pictures/background/generated.png
+  convert -font "Chilanka-Regular" -gravity Center -fill "#ffffff30" -pointsize 60 -draw "text -1480,-490 '$DATE'" /home/alexis/Pictures/background/generated.png /home/alexis/Pictures/background/generated.png
 
   feh --bg-fill /home/alexis/Pictures/background/generated.png;
   sleep 1m
