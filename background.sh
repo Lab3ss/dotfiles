f0caf8fa7d8d416618765340094a13c4528ca090
Alexis Besson <alexis@besson.tech>
2023-10-03 12:19:31 +0200

    trianglify blue bg with yellow borders

--- background.sh
+++ background.sh
@@ -6,8 +6,9 @@ while true; do
   echo $TIME
   echo $DAY
   echo $DATE
-  convert -font "Chilanka-Regular" -gravity Center -fill "#ffffff30" -pointsize 260 -draw "text -1480,-640 '$TIME'" /usr/share/backgrounds/warty-final-ubuntu.png /home/alexis/Pictures/background/generated.png
-  convert -font "Chilanka-Regular" -gravity Center -fill "#ffffff30" -pointsize 60 -draw "text -1480,-490 '$DATE'" /home/alexis/Pictures/background/generated.png /home/alexis/Pictures/background/generated.png
+  #convert -font "Chilanka-Regular" -gravity Center -fill "#ffffff30" -pointsize 260 -draw "text -1480,-640 '$TIME'" /usr/share/backgrounds/warty-final-ubuntu.png /home/alexis/Pictures/background/generated.png
+  convert -font "Chilanka-Regular" -gravity Center -fill "#ffffff50" -pointsize 180 -draw "text -820,-420 '$TIME'" /home/alexis/Development/dotfiles/theme-generator/trianglify.png /home/alexis/Pictures/background/generated.png
+  convert -font "Chilanka-Regular" -gravity Center -fill "#ffffff50" -pointsize 64 -draw "text -820,-300 '$DATE'" /home/alexis/Pictures/background/generated.png /home/alexis/Pictures/background/generated.png
 
   feh --bg-fill /home/alexis/Pictures/background/generated.png;
   sleep 1m
